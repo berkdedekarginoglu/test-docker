6a6f329ef542e31b631ed51eb6259158dfa8ba43
Berk Dedekargınoğlu <30373481+berkdedekarginoglu@users.noreply.github.com>
2022-09-10 20:54:08 +0300

    Add files via upload

--- web-app/app.py
+++ web-app/app.py
@@ -1,16 +1,36 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+from pymongo import MongoClient
+
 app = Flask(__name__)
 api = Api(app)
 
+client = MongoClient("mongodb://db:27017")
+
+db = client.aNewDB
+Workers = db["Workers"]
+
+Workers.insert({
+    "num_of_workers": 0
+})
+
+class Visit(Resource):
+    def get(self):
+        prev_num = Workers.find({})[0]['num_of_users']
+        new_num = prev_num + 1
+        Workers.update({},{"$set":{"num_of_workers":new_num}})
+        return str("Hello Wordker " + str(new_num))
 
 class Student(Resource):
     def get(self,name):
         return {'student': name}
 
 
+
+
 api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Visit,'/hello')
 
 
 if __name__ == "__main__":
