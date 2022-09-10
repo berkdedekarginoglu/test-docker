0c83ec9243fa3f6e65d7957fd495c12d145f1552
Berk Dedekargınoğlu <30373481+berkdedekarginoglu@users.noreply.github.com>
2022-09-10 23:24:14 +0300

    Add files via upload

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,5 @@
-import json
-
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -10,29 +7,31 @@ api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
 db = client.TwitterWorkers
-
 States = db["States"]
-
 States.insert_one({
     "worker_id": "185.203.67.209"
 })
 
-class GetWorkerInfo(Resource):
+class Add(Resource):
     def post(self):
-        data = request.get_json()
-        States = db["States"]
-        States.insert_one(data)
-        #States.update_one({"worker_id":"185.203.67.209"},{"$set":data})
-        return States.find({})
+        postedData = request.get_json()
+        retMap = {
+            'Message': 'ok',
+            'Status Code': 200
+        }
+        States.insert_one(postedData)
 
+        return jsonify(retMap)
+
+class Get(Resource):
     def get(self):
-        States = db["States"]
-        return jsonify(States.find({}))
+        prev_num = States.find({})[0]
+        return jsonify(prev_num)
 
 
-routes = ['/states/add','/states']
 
-api.add_resource(GetWorkerInfo,*routes)
+api.add_resource(Add, "/states/add")
+api.add_resource(Get, "/states")
 
 if __name__ == "__main__":
     app.run(port=5000,host='0.0.0.0')
