5205a8b01c3f8075c10d673b0851f2e0698ec2fd
Berk Dedekargınoğlu <30373481+berkdedekarginoglu@users.noreply.github.com>
2022-09-10 21:59:02 +0300

    Add files via upload

--- web-app/app.py
+++ web-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 from pymongo import MongoClient
@@ -7,19 +7,23 @@ app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
-db = client["aNewDB"]
-Workers = db["Workers"]
+db = client["TwitterWorkers"]
+TwitterWorkers = db["States"]
 
-Workers.insert_one({
-    "num_of_workers": 0
+TwitterWorkers.insert_one({
+    "worker_id": "185.203.67.209"
 })
 
-class Visit(Resource):
+class States(Resource):
+    def post(self):
+        data = request.get_json()
+        TwitterWorkers.update_one({"worker_id":"185.203.67.209"},data)
+        return {"success":True}
+
     def get(self):
-        prev_num = Workers.find({})[0]['num_of_users']
-        new_num = prev_num + 1
-        Workers.update({},{"$set":{"num_of_workers":new_num}})
-        return str("Hello Wordker " + str(new_num))
+        state = TwitterWorkers.find({"worker_id":"185.203.67.209"})
+        return state
+
 
 class Student(Resource):
     def get(self,name):
@@ -29,8 +33,8 @@ class Student(Resource):
 
 
 api.add_resource(Student, '/student/<string:name>')
-api.add_resource(Visit,'/hello')
-
+api.add_resource(States,'/states/add')
+api.add_resource(States,'/states')
 
 if __name__ == "__main__":
     app.run(port=5000,host='0.0.0.0')
