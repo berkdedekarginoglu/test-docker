f61a89494dbe4c5f12665040bd12632d7777dafe
Berk Dedekargınoğlu <30373481+berkdedekarginoglu@users.noreply.github.com>
2022-09-11 14:59:20 +0300

    Add files via upload

--- web-app/templates/script.js
+++ web-app/templates/script.js
@@ -1,4 +1,4 @@
-fetch("static/js/test.json")
+fetch("http://0.0.0.0:5000/workers")
 .then(function(response){
    return response.json();
 })
@@ -8,14 +8,28 @@ fetch("static/js/test.json")
    for(let worker of workers){
       out += `
          <tr>
-            <td> <img src='${worker.worker_ip}'> </td>
+            <td>${worker.worker_ip}</td>
             <td>${worker.total_success}</td>
             <td>${worker.total_scan}</td>
+            <td>${worker.last_success_date}</td>
             <td>${worker.success_rate}</td>
             <td>${worker.success}</td>
             <td>${worker.selected_country}</td>
             <td>${worker.selected_gsm}</td>
             <td>${worker.round_time}</td>
+            <td>${worker.current_step}</td>
+            <td>${worker.fail_attemps}</td>
+            <td>${worker.proxy.host}</td>
+            <td>${worker.proxy.port}</td>
+            <td>${worker.errors.guest_token_errors}</td>
+            <td>${worker.errors.login_flow_errors}</td>
+            <td>${worker.errors.username_errors}</td>
+            <td>${worker.errors.password_errors}</td>
+            <td>${worker.errors.acid_flow_errors}</td>
+            <td>${worker.errors.access_token_errors}</td>
+            <td>${worker.errors.password_change_errors}</td>
+            <td>${worker.errors.proxy_errors}</td>
+            <td>${worker.errors.exceptions}</td>
          </tr>
       `;
    }
