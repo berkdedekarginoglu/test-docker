714a2a5936bce55b35f4f7b0fa1bca3e25aaf25a
Berk Dedekargınoğlu <30373481+berkdedekarginoglu@users.noreply.github.com>
2022-09-13 22:10:52 +0300

    Add files via upload

--- web-app/static/js/agents_script.js
+++ web-app/static/js/agents_script.js
@@ -1,4 +1,5 @@
-let get_data = function() {
+agent_data = []
+let create_data = async function() {
    fetch("http://ec2-3-251-92-78.eu-west-1.compute.amazonaws.com/agents/scans")
    .then(function(response){
       return response.json();
@@ -8,38 +9,73 @@ let get_data = function() {
       let scan_out = "";
       let sortedInput = agents.slice().sort((a, b) => parseInt(b.success) - parseInt(a.success));
       for(let agent of sortedInput){
-         scan_out += `
-            <tr>
-               <td class="text-center">${agent.agent}</td>
-               <td class="text-center">${agent.success_rate.split('.')[0]}</td>
-               <td class="text-center">${agent.success}</td>
-               <td class="text-center">${agent.password_change_errors}</td>
-               <td class="text-center">${agent.current_step}</td>
-               <td class="text-center">${agent.scan_range}</td>
-               <td class="text-center">+${agent.country_code}</td>
-               <td class="text-center">${agent.gsm_code}</td>
-               <td class="text-center">${agent.gsm_errors}</td>
-               <td class="text-center">${agent.password_errors}</td>
-            </tr>
-         `;
+         
+         if(agent_data.some(item => item.country_code === agent.country_code &&
+            item.gsm_code === agent.gsm_code && item.current_step == agent.current_step)){
+               continue;
+            }
+         
+         var tr = document.createElement('tr');
+         var td1 = document.createElement('td').className("text-center");
+         var td2 = document.createElement('td').className("text-center");
+         var td3 = document.createElement('td').className("text-center");
+         var td4 = document.createElement('td').className("text-center");
+         var td5 = document.createElement('td').className("text-center");
+         var td6 = document.createElement('td').className("text-center");
+         var td7 = document.createElement('td').className("text-center");
+         var td8 = document.createElement('td').className("text-center");
+         var td9 = document.createElement('td').className("text-center");
+         var td10 = document.createElement('td').className("text-center");
+
+         var text1 = document.createTextNode(agent.agent);
+         var text2 = document.createTextNode(agent.success_rate.split('.')[0]);
+         var text3 = document.createTextNode(agent.success);
+         var text4 = document.createTextNode(agent.password_change_errors);
+         var text5 = document.createTextNode(agent.current_step);
+         var text6 = document.createTextNode(agent.scan_range);
+         var text7 = document.createTextNode(agent.country_code);
+         var text8 = document.createTextNode(agent.gsm_code);
+         var text9 = document.createTextNode(agent.gsm_errors);
+         var text10 = document.createTextNode(agent.password_errors);
+         
+         td1.appendChild(text1);
+         td2.appendChild(text2);
+         td3.appendChild(text3);
+         td4.appendChild(text4);
+         td5.appendChild(text5);
+         td6.appendChild(text6);
+         td7.appendChild(text7);
+         td8.appendChild(text8);
+         td9.appendChild(text9);
+         td10.appendChild(text10);
+
+         tr.appendChild(td1);
+         tr.appendChild(td2);
+         tr.appendChild(td3);
+         tr.appendChild(td4);
+         tr.appendChild(td5);
+         tr.appendChild(td6);
+         tr.appendChild(td7);
+         tr.appendChild(td8);
+         tr.appendChild(td9);
+         tr.appendChild(td10);
+
       };
-   
-      data_output_scan_flow.innerHTML = scan_out;
    });
 }
 
 
 let data_filter = function() {
    var input, filter, table, tr, td, i, txtValue;
-   input = document.getElementById("agent_scan_flow_table_filter");
-   filter = input.value.toUpperCase();
+   if(input){
+   filter = document.getElementById("agent_scan_flow_table_filter");
    table = document.getElementById("agent_scan_flow_table");
    tr = table.getElementsByTagName("tr");
    for (i = 0; i < tr.length; i++) {
      td = tr[i].getElementsByTagName("td")[6];
      if (td) {
        txtValue = td.textContent || td.innerText;
-       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+       if (txtValue.indexOf(filter) > -1) {
          tr[i].style.display = "";
        } else {
          tr[i].style.display = "none";
@@ -47,6 +83,7 @@ let data_filter = function() {
      }       
    }
  }
+}
 
 let get_stats = function() {
    fetch("http://ec2-3-251-92-78.eu-west-1.compute.amazonaws.com/agents/stats")
@@ -75,10 +112,8 @@ let get_stats = function() {
 
 get_stats();
 get_data();
-data_filter();
 
 setInterval(function() {
    get_stats();
-  get_data();
-  data_filter();
+   create_data();
  }, 10000);
