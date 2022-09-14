a41b8a19041dcb853ecfd6b5070a0fef7fbcdd89
unknown <bycybercoc@gmail.com>
2022-09-14 17:21:16 +0300

    test

--- web-app/app.py
+++ web-app/app.py
@@ -12,32 +12,16 @@ api = Api(app)
 client = MongoClient("mongodb://db:27017")
 selected_db = client.twitter_workers_db
 
-worker_states = selected_db.worker_states
+worker_states = selected_db.bandits_statistics
 worker_states.drop()
-worker_states = selected_db.worker_states
+worker_states = selected_db.bandits_statistics
 
-agent_stats = selected_db.agent_stats
-agent_stats.drop()
-agent_stats = selected_db.agent_stats
-
-agent_scans = selected_db.agent_scans
-agent_scans.drop()
-agent_scans = selected_db.agent_scans
-
-@app.route("/bandits/live")
-def live_bandits():
-    return render_template("bandits.html")
-
-@app.route("/agents/live")
-def live_agents():
-    return render_template("agents.html")
 
 @app.route("/")
 def index():
     return render_template("bandits.html")
 
 
-class Add(Resource):
     def post(self):
         try:
             postedData = request.get_json()
@@ -81,86 +65,9 @@ class UpdateBanditScan(Resource):
                 'error': str(e)
             }
 
-#agents
-
-class GetAgentScans(Resource):
-    def get(self):
-        try:
-            res = agent_scans.find({},{'_id':0})
-            return jsonify(list(res))
-        except Exception as e:
-            returnMap = {
-                'success': False,
-                'error': str(e)
-            }
-            return jsonify(returnMap)
-
-class GetAgentStats(Resource):
-    def get(self):
-        try:
-            res = agent_stats.find({},{'_id':0})
-            return jsonify(list(res))
-        except Exception as e:
-            returnMap = {
-                'success': False,
-                'error': str(e)
-            }
-            return jsonify(returnMap)
-
-class UpdateAgentScan(Resource):
-    def post(self):
-        try:
-            postedData = request.get_json()
-            agent_scans.update_one({"country_code":postedData['country_code'],
-                                      "gsm_code":postedData['gsm_code'],
-                                      "current_step":postedData['current_step']},{"$set":postedData}, upsert=True)
-
-            agent_stats.update_one({"agent":postedData["agent"]},{"$set": {
-                "agent":postedData["agent"],
-                "last_info":str(datetime.datetime.now()),
-                "current_country":postedData["country_code"],
-                "current_gsm":postedData["gsm_code"],
-                "current_step":postedData["current_step"],
-                "proxy_host":postedData["proxy_host"],
-                "proxy_port":postedData["proxy_port"]
-            }}, upsert=True)
-            retunMap = {
-                'success': True
-            }
-            return jsonify(retunMap)
-        except Exception as e:
-            retunMap = {
-                'success': False,
-                'error': str(e)
-            }
-            return jsonify(retunMap)
-
-class UpdateAgentState(Resource):
-    def post(self):
-        try:
-            postedData = request.get_json()
-
-            agent_stats.update_one({"agent":postedData["agent"]},{"$set": {
-               "state":postedData["state"]
-            }}, upsert=True)
-            retunMap = {
-                'success': True
-            }
-            return jsonify(retunMap)
-        except Exception as e:
-            retunMap = {
-                'success': False,
-                'error': str(e)
-            }
-            return jsonify(retunMap)
-
 api.add_resource(UpdateBanditScan, "/bandits")
-api.add_resource(UpdateAgentScan, "/agents/scans")
-api.add_resource(UpdateAgentScan, "/agents/state")
-
 api.add_resource(Get, "/bandits")
-api.add_resource(GetAgentScans, "/agents/scans")
-api.add_resource(GetAgentStats, "/agents/stats")
+
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')
