9297f640d4cb427088a6240225828ad960376ca2
unknown <bycybercoc@gmail.com>
2022-09-14 21:14:06 +0300

    test

--- web-app/app.py
+++ web-app/app.py
@@ -1,48 +1,110 @@
 import datetime
-
+from unittest import result
 from flask import Flask, request, jsonify, render_template
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 api = Api(app)
 
 
-
-client = MongoClient("mongodb://db:27017")
-selected_db = client.twitter_workers_db
-
-worker_states = selected_db.bandits_statistics
-worker_states.drop()
-worker_states = selected_db.bandits_statistics
-
-
-@app.route("/")
+### WEB ####
+@app.route('/')
 def index():
-    return render_template("bandits.html")
+    return render_template('bandits-scan-statistics.html')
 
 
-    def post(self):
+
+### MONGO ####
+class MongoDB:
+    def __init__(self,db_name,column_name) -> None:
+        self.client =  MongoClient('mongodb://db:27017')
+        self.selected_db = self.client[db_name]
+        self.selected_column = self.selected_db[column_name]
+
+    def get(self,query):
         try:
-            postedData = request.get_json()
-            worker_states.insert_one(postedData)
-            returnMap = {
-                'success': True
-            }
-            return jsonify(returnMap)
+            res = self.selected_column.find(query,{'_id':0})
+
+            return jsonify({
+                'success':True,
+                'data':list(res)
+                })
 
         except Exception as e:
             returnMap = {
                 'success': False,
                 'error': str(e)
             }
-            return jsonify(returnMap)
+        return jsonify(returnMap)
 
-class Get(Resource):
-    def get(self):
+
+    def insertOne(self,data):  
         try:
-            res = worker_states.find({},{'_id':0})
+            self.selected_column.insertOne(data)
+            return jsonify({
+                'success': True
+            })
+
+        except Exception as e:
+            return jsonify({
+                'success': False,
+                'error': str(e)
+            })
+
+
+    def updateOne(self,query,data):
+        try:
+            self.selected_column.update_one(query,{'$set':data}, upsert=True)
+            return jsonify({
+                'success': True
+            })
+        except Exception as e:
+           return jsonify({
+                'success': False,
+                'error': str(e)
+            })
+
+
+
+
+
+### Bandit-API ###
+
+
+class BanditsStatistics(Resource):
+    def __init__(self):
+        self.mongo = MongoDB('twitter_banditos','bandits_scan_statistics')
+
+    def post(self): # Add New Statistic
+        try:
+            postedData = request.get_json()
+            result = json.loads(self.mongo.insertOne(postedData))
+
+            if result['success']:
+
+                return jsonify({
+                'success': True
+                })
+
+            return jsonify({
+                'success': False,
+                'error': result['error']
+            })
+
+        except Exception as e:
+            return jsonify({
+                'success':False,
+                'error':str(e)
+            })
+
+
+    def get(self): # Get All Statistics
+        try:
+            res = self.mongo.get({},{'_id':0})
             return jsonify(list(res))
+
         except Exception as e:
             returnMap = {
                 'success': False,
@@ -50,24 +112,33 @@ class Get(Resource):
             }
             return jsonify(returnMap)
 
-class UpdateBanditScan(Resource):
-    def post(self):
+
+    def put(self): # Update Statistic
         try:
+
             postedData = request.get_json()
-            worker_states.update_one({"worker_ip":postedData['worker_ip']},{"$set":postedData}, upsert=True)
-            retunMap = {
-                'success': True
-            }
-            return jsonify(retunMap)
+            result = json.loads(self.mongo.updateOne({"bandit":postedData["bandit"]},postedData["data"]))
+
+            if result["success"]:
+
+                return jsonify({
+                    'success':True
+                })
+
+            return jsonify({
+                'success':False,
+                'error':result['error']
+            })
+
         except Exception as e:
-            retunMap = {
-                'success': False,
-                'error': str(e)
-            }
 
-api.add_resource(UpdateBanditScan, "/bandits")
-api.add_resource(Get, "/bandits")
+            return jsonify({
+                "success":False,
+                "error":str(e)
+            })
+
+api.add_resource(BanditsStatistics, '/api/bandits/statistics')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
